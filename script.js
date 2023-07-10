36c61bd01f893cbec7429650636dec665f755cda
Chxshire22 <akuindra22@gmail.com>
2023-07-10 14:12:38 +0800

    updated card comp

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
+// For collapsibles
 
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+
+// Get the modal
+let modal = document.getElementById("myModal"); //this needs to be an ID because if you open up by class any and all with that class will open and may crash the browser 
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+let img = document.getElementById("myImg");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get modal content
+let modalContent = document.getElementsByClassName("modal-content");
+
+const closeModal = () => {
+  modal.style.display = "none";
+};
+
+const notClickModalContent = (event) => {
+  if (event.target === modal) {
+          console.log("test works1");
+          closeModal();
+    }
+  }
+
+// event listener if not click modal content
+window.addEventListener("click", notClickModalContent);
+
+
+//TABS functionality 
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
+
+
+$(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true,
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
