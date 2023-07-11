8d90a84838f1c4323cd2a1557132f0a9c8131d34
Chxshire22 <akuindra22@gmail.com>
2023-07-11 23:41:21 +0800

    working on carousel comp

--- script.js
+++ script.js
@@ -39,37 +39,3 @@ const notClickModalContent = (event) => {
 window.addEventListener("click", notClickModalContent);
 
 
-//TABS functionality 
-
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-} 
-
-
-$(".custom-carousel").owlCarousel({
-  autoWidth: true,
-  loop: true,
-});
-$(document).ready(function () {
-  $(".custom-carousel .item").click(function () {
-    $(".custom-carousel .item").not($(this)).removeClass("active");
-    $(this).toggleClass("active");
-  });
-});
